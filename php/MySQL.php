053d8e79195d2ec446788b919efdedf7e1cdfcd9
HtooThetNaung <92380477+Austin365Dev@users.noreply.github.com>
2023-11-18 23:09:47 +0630

    Add files under Libs/Database

---
+++ php/MySQL.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Libs\Database;
+
+class MySQL
+{
+    public function connect()
+    {
+        echo "MySQL connect <br>";
+    }
+}
---
+++ php/UsersTable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Libs\Database;
+
+class UsersTable
+{
+    public function insert()
+    {
+        echo "UsersTable insert <br>";
+    }
+}
